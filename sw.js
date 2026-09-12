b6cb4e750d3ada1228991e59b158509ea9f3e483
Ilyasabdullah <84226261+Ilyasabdullah@users.noreply.github.com>
2026-09-12 13:23:06 +0900

    Add service worker with caching functionality
    Implement service worker for caching and fetching resources.

--- sw.js
+++ sw.js
@@ -1,1 +1,37 @@
+const CACHE_NAME = 'presensi-kiosk-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://unpkg.com/html5-qrcode',
+  'https://cdn.pixabay.com/download/audio/2021/08/04/audio_0625c1539c.mp3'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    }).catch(() => {
+      return caches.match('/index.html');
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
